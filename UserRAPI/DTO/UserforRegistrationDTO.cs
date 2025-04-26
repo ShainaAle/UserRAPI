54673174327d043812c797e82bffe1fc3d78adc7
Shaina Alexandra Xochitiotzi Rojas <shainalexandraxo@gmail.com>
2025-04-26 10:43:16 -0500

    Agrega type y validacion

--- UserRAPI/DTO/UserforRegistrationDTO.cs
+++ UserRAPI/DTO/UserforRegistrationDTO.cs
@@ -12,6 +12,8 @@ namespace UserRAPI.DTO
         [Required(ErrorMessage = "Password is required.")]
         public string? Password { get; set; }
         [Compare("Password", ErrorMessage = "Passwords do not match.")]
-        public string? ConfirmPassword { get; set; }    
+        public string? ConfirmPassword { get; set; }
+        [AllowedTypes(new[] { "student", "admin", "teacher" }, ErrorMessage = "Type must be 'student', 'admin', or 'teacher'.")]
+        public string? Type { get; set; }
     }
 }
